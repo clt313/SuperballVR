f10da30e221e12687d335931ff7fc4027850ce58
unknown <franky.sanchez@ufl.edu>
2022-11-01 13:09:04 -0400

    Finished implementing saving settings

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -12,51 +12,49 @@ public class SettingsMenu : MonoBehaviour
     public Slider masterSlider;
     public Slider musicSlider;
     public Slider soundSlider;
-    public Dropdown qualityDropdown;
+    public TMPro.TMP_Dropdown qualityDropdown;
     const string prefName = "optionvalue";
 
     void Awake() {
-        // qualityDropdown.onValueChanged.AddListener(new UnityAction<int>(index =>
-        //     {
-        //         PlayerPrefs.SetInt(prefName, qualityDropdown.value);
-        //         PlayerPrefs.Save();
-        //     }
-        // ));
+        qualityDropdown.onValueChanged.AddListener(new UnityAction<int>(index =>
+            {
+                PlayerPrefs.SetInt(prefName, qualityDropdown.value);
+                PlayerPrefs.Save();
+            }
+        ));
 
     }
 
     void Start() {
-        masterSlider.value = PlayerPrefs.GetFloat("MasterVolume", 1f);
-        audioMixer.SetFloat("MasterVolume", PlayerPrefs.GetFloat("MasterVolume"));
-
         musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
-        audioMixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("MusicVolume"));
+        //SetMusicVolume(musicSlider.value);
 
         soundSlider.value = PlayerPrefs.GetFloat("SoundVolume", 1f);
-        audioMixer.SetFloat("SoundVolume", PlayerPrefs.GetFloat("SoundVolume"));
+        //SetSoundVolume(soundSlider.value);
 
-        //qualityDropdown.value = PlayerPrefs.GetInt(prefName, 3);
+        masterSlider.value = PlayerPrefs.GetFloat("MasterVolume", 1f);
+        SetMasterVolume(masterSlider.value);
+
+        qualityDropdown.value = PlayerPrefs.GetInt(prefName, 3);
     }
 
     public void SetMasterVolume (float volume) {
-        PlayerPrefs.SetFloat("MasterVolume", volume);
-        audioMixer.SetFloat("MasterVolume", volume);
-
-        PlayerPrefs.SetFloat("MusicVolume", volume);
-        audioMixer.SetFloat("MusicVolume", volume);
-
-        PlayerPrefs.SetFloat("SoundVolume", volume);
-        audioMixer.SetFloat("SoundVolume", volume);
+        PlayerPrefs.SetFloat("MasterVolume",volume);
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+        SetMusicVolume(musicSlider.value);
+        SetSoundVolume(soundSlider.value);
     }
 
     public void SetMusicVolume (float volume) {
         PlayerPrefs.SetFloat("MusicVolume", volume);
-        audioMixer.SetFloat("MusicVolume", volume);
+        volume = volume*masterSlider.value;
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
     }
 
     public void SetSoundVolume (float volume) {
         PlayerPrefs.SetFloat("SoundVolume", volume);
-        audioMixer.SetFloat("SoundVolume", volume);
+        volume = volume*masterSlider.value;
+        audioMixer.SetFloat("SoundVolume", Mathf.Log10(volume) * 20);
     }
 
     public void SetQuality(int qualityIndex) {
