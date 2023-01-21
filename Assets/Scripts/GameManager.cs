e71b6b6a953855e80317b47e21df453e1604a767
haroonsyed <haroonaminsyed@gmail.com>
2023-01-20 21:50:44 -0500

    Added fields for game rules

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,21 +6,38 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        BallEvents.ballBounceEvent.AddListener(handleBallBounce);
-    }
+  public int maxBounces = 1;
+  public int maxPasses = 1;
+  public int maxGameTimeSeconds = 10 * 60;
+  public int maxScore = 5;
+  public GameObject teamOneCourt;
+  public GameObject teamTwoCourt;
+  public GameObject net;
 
-    // Update is called once per frame
-    void Update()
-    {
+  private int scoreTeamOne = 0;
+  private int scoreTeamTwo = 0;
 
-    }
+  private bool isBallInPlay = false;
+  private int currentBounces = 0;
+  private int currentPasses = 0;
 
-    void handleBallBounce(GameObject listenedBall, GameObject listenedCollidedObject)
-    {
-        Debug.Log("Game manager detected a ball bounce with: " + listenedCollidedObject.name);
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
+    BallEvents.ballBounceEvent.AddListener(handleBallBounce);
+  }
+
+  // Handles performing actions as a result of collisions
+  // Namely starting and stopping the game
+  void Update()
+  {
+
+  }
+
+  // Handles anything to do with collisions
+  void handleBallBounce(GameObject listenedBall, GameObject listenedCollidedObject)
+  {
+    Debug.Log("Game manager detected a ball bounce with: " + listenedCollidedObject.name);
+  }
 
 }
