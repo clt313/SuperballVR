821f93d4c79208b61a4d881d54bbb1aa3ba374e8
FSanchez-UF <73603246+FSanchez-UF@users.noreply.github.com>
2023-02-16 17:31:32 -0500

    Improved AI scripts (#77)
    * Fixed Vive button layout
    
    * Improved AI scripts

--- Assets/Scripts/SpaceKyle/KyleBT.cs
+++ Assets/Scripts/SpaceKyle/KyleBT.cs
@@ -4,8 +4,8 @@ using BehaviorTree;
 public class KyleBT : Tree
 {
     public UnityEngine.Transform[] waypoints;
-    public static float speed = 2f;
-    public static float fovRange = 6f;
+    public static float speed = 1f;
+    public static float fovRange = 10f;
     public static float hitRange = 1f;
 
     protected override Node SetupTree()
