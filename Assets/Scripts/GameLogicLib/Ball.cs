4035bcb52f8e8a8db003e449d7ee8105a504f6a9
Johnny <58481830+jgionti@users.noreply.github.com>
2023-03-08 15:07:36 -0500

    Added ball effects (#107)
    * Added trail effect to ball
    
    * Removed unused GameManager input
    
    * Added particle effect to Ball on round end
    
    * Added ball glow effect + light

--- Assets/Scripts/GameLogicLib/Ball.cs
+++ Assets/Scripts/GameLogicLib/Ball.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+  public Material glowMaterial;
+
   // Start is called before the first frame update
   void Start()
   {
+    // Decide which material to use based on map
+    // Use glow material on Map2, otherwise default
+    if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Map2") {
+      gameObject.GetComponent<MeshRenderer>().material = glowMaterial;
+      gameObject.GetComponent<Light>().enabled = true;
+    }
+
+    // Delete self on round end
     GameEvents.roundEndEvent.AddListener(deleteSelf);
   }
 
@@ -24,6 +34,11 @@ public class Ball : MonoBehaviour
 
   void deleteSelf()
   {
+    // Create particle effect where ball ended at
+    GameObject ballEmitter = GameObject.Find("Particle Effects/BallEmitter");
+    GameObject particle = GameObject.Instantiate(ballEmitter, gameObject.transform.position, Quaternion.identity);
+    particle.GetComponent<ParticleSystem>().Play();
+    
     Object.Destroy(this.gameObject);
   }
 
