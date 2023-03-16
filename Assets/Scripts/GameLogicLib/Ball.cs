aaf69c956bb4526b94da1659c95009a86803d2e0
haroonsyed <haroonaminsyed@gmail.com>
2023-03-16 05:11:34 -0400

    Added function to get expected ball collision point on floor.

--- Assets/Scripts/GameLogicLib/Ball.cs
+++ Assets/Scripts/GameLogicLib/Ball.cs
@@ -11,7 +11,8 @@ public class Ball : MonoBehaviour
   {
     // Decide which material to use based on map
     // Use glow material on Map2, otherwise default
-    if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Map2") {
+    if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Map2")
+    {
       gameObject.GetComponent<MeshRenderer>().material = glowMaterial;
       gameObject.GetComponent<Light>().enabled = true;
     }
@@ -26,6 +27,28 @@ public class Ball : MonoBehaviour
 
   }
 
+  Vector3 getExpectedCollisionPosition(float yTarget)
+  {
+
+    Vector3 ballPosition = GetComponent<Rigidbody>().position;
+    Vector3 ballVelocity = GetComponent<Rigidbody>().velocity;
+
+    // Use d = vit + 0.5at^2 to get time in y direction till target
+    // Using quadratic equation this gives t = (- vi +/- sqrt(vi**2 + 2*a*deltaD)) / a
+    // Using that we just need to multiply t on vx and vz for final position on court
+    float vi = ballVelocity.y;
+    float deltaD = yTarget - ballPosition.y;
+    float a = Physics.gravity.y;
+
+    float t1 = (-vi + Mathf.Sqrt(vi * vi + 2 * a * deltaD)) / a;
+    float t2 = (-vi - Mathf.Sqrt(vi * vi + 2 * a * deltaD)) / a;
+    float t = Mathf.Max(t1, t2); // We only care about the positive t one
+
+    float collisionX = ballPosition.x + ballVelocity.x * t;
+    float collisionZ = ballPosition.z + ballVelocity.z * t;
+    return new Vector3(collisionX, yTarget, collisionZ);
+  }
+
   // Dispatch event
   private void OnCollisionEnter(Collision collision)
   {
@@ -38,7 +61,7 @@ public class Ball : MonoBehaviour
     GameObject ballEmitter = GameObject.Find("Particle Effects/BallEmitter");
     GameObject particle = GameObject.Instantiate(ballEmitter, gameObject.transform.position, Quaternion.identity);
     particle.GetComponent<ParticleSystem>().Play();
-    
+
     Object.Destroy(this.gameObject);
   }
 
